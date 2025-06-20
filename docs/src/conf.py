ede87461fd29f213440a22d3971871b1b8a0328b
Guillaume Jourjon <guillaume.jourjon@data61.csiro.au>
2025-06-20 14:16:06 +1000

    REL-2198 preparation of P4 code version 1.0.0 syncing with the rest of the LOW CBF

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -49,9 +49,9 @@ copyright = "2020-2022, Guillaume Jourjon"
 author = "Guillaume Jourjon"
 
 # The short X.Y version
-version = '0.6.0'
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags
-release = '0.6.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
